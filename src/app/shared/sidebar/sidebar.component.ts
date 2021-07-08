35c7419533eb563968b044e294e6bc6d1093da21
Xelac <68864369+Xelacnnister@users.noreply.github.com>
2021-07-08 18:13:43 +0200

    Componente Sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -5,8 +5,13 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: [
+    `
+      li{
+        cursor: pointer;
+      }
+    `
   ]
-})
+})//ponemos el style del pointer aqui para que solo haga objetivo a los "li" del componente Sidebar
 export class SidebarComponent implements OnInit {
 
   constructor() { }
