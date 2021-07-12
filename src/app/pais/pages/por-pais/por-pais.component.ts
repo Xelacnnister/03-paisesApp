16fec1413b85427c8f449f00aeeed1476e68e2c0
Xelac <68864369+Xelacnnister@users.noreply.github.com>
2021-07-12 20:07:04 +0200

    Manejo de errores

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -10,15 +10,19 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
 
   termino: string = '';
+  hayError: boolean = false;
 
   constructor( private paisService: PaisService) { }//inyectamos el servicio en el constructor para poder usarlo
 
   buscar(){//postea el término buscado
+    this.hayError = false;//mediante el ngIf NO muestra el mensaje de error
     console.log(this.termino);
     
     this.paisService.buscarPais( this.termino )//para que un Obbservable se dispare debo tener por lo menos un subscribe
-      .subscribe( resp => {
+      .subscribe( (resp) => {
         console.log(resp);
+      }, (err) => { //cuando no encuentre el termino buscado (error 404), mostrará lo siguiente:
+        this.hayError = true;//mediante el ngIf muestra el mensaje de error
         
       })
   }
