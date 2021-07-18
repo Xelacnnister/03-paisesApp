da6097b10123b14f498a0aafd5f37081a1cdb21a
Xelac <68864369+Xelacnnister@users.noreply.github.com>
2021-07-18 18:52:23 +0200

    RxJs - SwitchMap

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -17,15 +18,27 @@ export class VerPaisComponent implements OnInit {
 
   ngOnInit(): void { 
 
-    this.activatedRoute.params //convierte en un Observable la ruta activa como parametro. / En la consola pararece como id: por haberlo indicado así en el routing.module (/:id)
-      .subscribe( ({id}) => { // desestructuramos el param para extraer directamente el id ({id})
-        console.log( id );// Antes de desestructurar el params en el subscribe( params -> ({id}) ), usariamos params.id
+    this.activatedRoute.params //accedemos al Observable que contierne los parámetros
+      .pipe(//para usar los operadores de rxjs./ dentro podemos especificar cualquier cantidad de operadores que van a trabajar con el producto del Observable anterior, en este caso: params
+        switchMap( ({ id }) => this.paisService.getPaisPorAlpha( id ) )//switchMap es uno de los oeradores de transformación, recibe un Observable( ({ id })) y devuelve otro Observable( this.paisService.getPaisPorAlpha( id ) )
+      )
+        .subscribe( resp => { //subscribimos al Observable params
+          console.log( resp );
+          
+      });
+      
+
+    /* Versión larga con doble .subscribe */ 
+
+    // this.activatedRoute.params //convierte en un Observable la ruta activa como parametro. / En la consola pararece como id: por haberlo indicado así en el routing.module (/:id)
+    //   .subscribe( ({id}) => { // desestructuramos el param para extraer directamente el id ({id})
+    //     console.log( id );// Antes de desestructurar el params en el subscribe( params -> ({id}) ), usariamos params.id
         
-        this.paisService.getPaisPorAlpha( id ) //Creamos otro Observable para opteener la información del país./ creamos el método getPaisPorAlpha() en el pais.service.ts
-        .subscribe( pais => { //nos suscribimos al id para tener esa información
-          console.log( pais );
-        })
-      })
+    //     this.paisService.getPaisPorAlpha( id ) //Creamos otro Observable para opteener la información del país./ creamos el método getPaisPorAlpha() en el pais.service.ts
+    //     .subscribe( pais => { //nos suscribimos al id para tener esa información
+    //       console.log( pais );
+    //     })
+    //   })
 
   }
 
