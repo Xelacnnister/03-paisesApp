35b22ae336825d0e3f4f65f3c8122c92756067d5
Xelac <68864369+Xelacnnister@users.noreply.github.com>
2021-07-20 10:31:29 +0200

    Mostrar sugerencias al escribir - autocomplete

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -7,6 +7,11 @@ import { Country } from '../../interfaces/pais-interface';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+      li{
+        cursor: pointer;
+      }
+    `
   ]
 })
 export class PorPaisComponent {
@@ -14,12 +19,16 @@ export class PorPaisComponent {
   termino : string = '';
   hayError: boolean = false;
   paises  : Country[] = [];
- 
+
+  paisesSugeridos   : Country[] = [];
+  mostrarSugerencias: boolean = false;//variable de control para mostrar o no las sugerencias
+
 
   constructor( private paisService: PaisService) { }//inyectamos el servicio en el constructor para poder usarlo
 
   buscar( termino: string ){//recibo el término de busqueda de tipo string
     
+    this.mostrarSugerencias = false;
     this.hayError = false;//mediante el ngIf NO muestra el mensaje de error
     this.termino  = termino;//this.termino es igual al termino que recibo como parametro
     
@@ -27,7 +36,7 @@ export class PorPaisComponent {
       .subscribe( (paises) => {//cambiamos el "resp" por "paises" ya que ahora que es de tipado Country[], tiene más sentido
         console.log(paises);
 
-        this.paises = paises;
+        this.paises = paises;//asignamos los resultados recibidos del subscribe en nuestra variable paises de este componente
 
       }, (err) => { //cuando no encuentre el termino buscado (error 404), mostrará lo siguiente:
         this.hayError = true;//mediante el ngIf muestra el mensaje de error
@@ -37,7 +46,18 @@ export class PorPaisComponent {
 
   sugerencias ( termino: string ) {
     this.hayError = false;
-    //TODO: crear sugerencias
+    this.termino = termino;//asignamos el termino recibido en nuestra variable this.termino
+    this.mostrarSugerencias = true;//cuando se haga funcionar este método se mostraran las sugerencias
+    
+    this.paisService.buscarPais( termino )//aplicamos el método buscarPais() del servicio paisService
+      .subscribe( 
+        paises=> this.paisesSugeridos = paises.splice(0,5),//.splice muestra solo los 5 primeros paise que coinciden con la búsqueda
+        (err) => this.paisesSugeridos = [] //Si no hay ningun pais con la búsqueda, vaciamos el array
+      );
+  }
+
+  buscarSugerido( termino: string ){
+    this.buscar( termino ); //aplicamos el método buscar pasandole el termino recibido en el método buscarSugerido()
   }
 
 }
